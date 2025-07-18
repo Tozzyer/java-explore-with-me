ba02aea3a82804ce0657adb3dd7c7376566d42dd
Home <teh-exe@ya.ru>
2025-07-18 15:13:52 +0400

    Structure 2

---
+++ stats-service/stats-server/src/main/java/ru/practicum/stats/model/EndpointHit.java
@@ -0,0 +1,28 @@
+ackage ru.practicum.stats.model;
+
+import lombok.*;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "endpoint_hits")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EndpointHit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String app;
+
+    private String uri;
+
+    private String ip;
+
+    @Column(name = "timestamp")
+    private LocalDateTime timestamp;
+}
