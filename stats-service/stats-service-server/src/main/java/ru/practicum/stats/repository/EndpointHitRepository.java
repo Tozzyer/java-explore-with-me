94bc2f203fe9310a1bdbb30a54648ba1730239d4
Home <teh-exe@ya.ru>
2025-08-07 20:44:31 +0700

    WIP2

--- stats-service/stats-service-server/src/main/java/ru/practicum/stats/repository/EndpointHitRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.stats.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.practicum.stats.model.EndpointHit;
-
-public interface EndpointHitRepository extends JpaRepository<EndpointHit, Long> {
-}
