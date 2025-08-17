e6a6bc64feae3d5eec797afab2eca997df5c25c9
Home <teh-exe@ya.ru>
2025-08-17 18:45:55 +0500

    Этап 2

--- main-service/src/main/resources/schema.sql
+++ main-service/src/main/resources/schema.sql
@@ -2,12 +2,22 @@ DROP TABLE IF EXISTS compilation_events CASCADE;
 DROP TABLE IF EXISTS compilations CASCADE;
 DROP TABLE IF EXISTS participation_requests CASCADE;
 
-CREATE TABLE IF NOT EXISTS participation_requests (
-                                                      id           BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-                                                      event_id     BIGINT NOT NULL,
-                                                      requester_id BIGINT NOT NULL,
-                                                      status       VARCHAR(100) NOT NULL,
-    created      TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    CONSTRAINT fk_participation_event FOREIGN KEY (event_id) REFERENCES events(id),
-    CONSTRAINT fk_participation_user FOREIGN KEY (requester_id) REFERENCES users(id)
-    );
+CREATE TABLE users (
+                       id              BIGSERIAL PRIMARY KEY,
+                       username        VARCHAR(255) NOT NULL UNIQUE,
+                       email           VARCHAR(255) NOT NULL UNIQUE,
+                       password_hash   VARCHAR(255) NOT NULL,
+                       created_at      TIMESTAMP DEFAULT now(),
+                       updated_at      TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE participation_requests (
+                                        id              BIGSERIAL PRIMARY KEY,
+                                        event_id        BIGINT NOT NULL,
+                                        requester_id    BIGINT NOT NULL,
+                                        status          VARCHAR(50) DEFAULT 'PENDING',
+                                        created_at      TIMESTAMP DEFAULT now(),
+
+                                        CONSTRAINT fk_participation_event FOREIGN KEY (event_id) REFERENCES events(id),
+                                        CONSTRAINT fk_participation_user FOREIGN KEY (requester_id) REFERENCES users(id)
+);
